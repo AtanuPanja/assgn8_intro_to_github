d1352f8209680fcc5bcf51273e7d1eca8c9c6f6a
Atanu Panja <74297632+AtanuPanja@users.noreply.github.com>
2023-05-27 12:40:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello')
+  res.send('Uploading a website from GitHub to EC2')
 })
 
 app.listen(4000, ()=>{
     console.log("Started server");
-}
+  }
 )
